a42783a0962f0811b0488f7bc1bd462adc6e77b2
lindsaybrose <lindsaybrocken@hotmail.com>
2024-11-24 13:14:29 +0000

    f

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import '../src/'
 import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
